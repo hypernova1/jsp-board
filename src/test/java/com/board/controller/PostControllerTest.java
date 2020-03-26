0fb8ba7b2db6a1efc3a1a73dc145f52a3fb8a05b
sam <chtlstjd01@gmail.com>
2020-03-26 22:22:11 +0900

    테스트 추가

---
+++ src/test/java/com/board/controller/PostControllerTest.java
@@ -0,0 +1,14 @@
+package com.board.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostControllerTest {
+
+    @Test
+    void test() {
+        System.out.println("test");
+    }
+
+}
